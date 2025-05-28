921653f8ff2009484608e5ea6cb25247d27ba530
PabloSantos-CG <comercial-pablo@outlook.com>
2025-05-27 22:03:16 -0300

    feat: add class routes

--- src/Routes/Routes.php
+++ src/Routes/Routes.php
@@ -4,3 +4,50 @@ namespace App\Routes;
 
 
 class Routes
+{
+    private static array $routes = [];
+
+    private function __construct() {}
+
+    public static function get(string $path, callable $callable)
+    {
+        Routes::$routes[] = [
+            'method' => 'GET',
+            'path' => $path,
+            'controller' => $callable,
+        ];
+    }
+
+    public static function post(string $path, callable $callable)
+    {
+        Routes::$routes[] = [
+            'method' => 'POST',
+            'path' => $path,
+            'controller' => $callable,
+        ];
+    }
+
+    public static function put(string $path, callable $callable)
+    {
+        Routes::$routes[] = [
+            'method' => 'PUT',
+            'path' => $path,
+            'controller' => $callable,
+        ];
+    }
+
+    public static function delete(string $path, callable $callable)
+    {
+        Routes::$routes[] = [
+            'method' => 'DELETE',
+            'path' => $path,
+            'controller' => $callable,
+        ];
+    }
+
+
+    public static function routes(): array
+    {
+        return Routes::$routes;
+    }
+}
