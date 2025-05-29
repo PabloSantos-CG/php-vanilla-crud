60862ec8f19a0180effee7c46df2c8ad6eb6d9df
PabloSantos-CG <comercial-pablo@outlook.com>
2025-05-28 21:29:12 -0300

    update

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
+require_once 'vendor/autoload.php';
 
-if (isset($_GET['url']) && $_GET['url'] === 'ping') {
-    header('Content-Type: application/json');
-    echo json_encode(['ping' => 'pong']);
+use App\Database\Database;
+
+class PrintToHello {
+    public static function run($a) {
+        echo 'Say Hello'.$a;
+    }
 }
+
+// Duas maneiras de fazer isso:
+// Primeria:
+$rts = ['/' => fn($a) => PrintToHello::run($a)];
+// Segunda:
+$rts = ['/' => [], ]
+
+
+// echo var_dump($rts['rt']);
+call_user_func($rts['rt'], ' concatenação mopai');
+
+echo '<pre>';
+var_dump($rts);
+
+// echo print_r(Database::connect());
+// echo var_dump(file_get_contents('php://input'));
+
+
