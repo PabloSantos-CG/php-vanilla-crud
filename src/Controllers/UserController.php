45aae5c6f70ad4102836d9e0acf40781671b01d1
PabloSantos-CG <comercial-pablo@outlook.com>
2025-05-28 21:28:56 -0300

    feat controller skeleton

---
+++ src/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController
+{
+    public static function getAllUsers() {}
+    public static function getUserById() {}
+    public static function addUser() {}
+    public static function updateUser() {}
+    public static function removeUser() {}
+}
+
