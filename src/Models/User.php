074b941d6cbb4a6ec471134fdff3a6009ac28235
PabloSantos-CG <comercial-pablo@outlook.com>
2025-05-31 18:15:05 -0300

    feature: add UserModel

---
+++ src/Models/User.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+use App\database\Database;
+use PDO;
+
+class User
+{
+    /** 
+     * @return array<int, array{id: int, username: string, email: string, password: string}> | array{} 
+     */
+    public static function getAll(): array
+    {
+        $sql = 'SELECT * FROM users';
+
+        $pdo = Database::connect();
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    /** 
+     * @return array{id: int, username: string, email: string, password: string} | array{} 
+     */
+    public static function getById(string $id): array
+    {
+        $sql = 'SELECT * FROM users WHERE id= ?';
+
+        $pdo = Database::connect();
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->execute([$id]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public static function create(string $username, string $email, string $password): bool
+    {
+        $sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+
+        $pdo = Database::connect();
+        $stmt = $pdo->prepare($sql);
+
+        return $stmt->execute([$username, $email, $password]);
+    }
+
+    public static function update(
+        string $id,
+        ?string $username = null,
+        ?string $email = null,
+        ?string $password = null,
+    ): bool {
+        $sql = '
+            UPDATE users 
+            SET 
+                username= COALESCE(:username, username),
+                email= COALESCE(:email, email),
+                password= COALESCE(:password, password)
+            WHERE id= :id
+        ';
+
+        $pdo = Database::connect();
+        $stmt = $pdo->prepare($sql);
+
+        return $stmt->execute([
+            'username' => $username,
+            'email' => $email,
+            'password' => $password,
+            'id' => $id,
+        ]);
+    }
+
+    public static function remove(string $id): bool
+    {
+        $sql = 'DELETE FROM users WHERE id= ?';
+
+        $pdo = Database::connect();
+        $stmt = $pdo->prepare($sql);
+
+        return $stmt->execute([$id]);
+    }
+}
