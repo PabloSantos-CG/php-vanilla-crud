340cc077c5ac33cd4c0518cef4b35254175dcaa3
PabloSantos-CG <comercial-pablo@outlook.com>
2025-05-31 19:57:18 -0300

    refactor: rename method

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController
 {
     public static function getAllUsers(Request $req, Response $res) {}
     public static function getUserById(Request $req, Response $res, string $id) {}
-    public static function addUser(Request $req, Response $res) {}
+    public static function createUser(Request $req, Response $res) {}
     public static function updateUser(Request $req, Response $res, string $id) {}
     public static function removeUser(Request $req, Response $res, string $id) {}
 }
