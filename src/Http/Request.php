da97d71bbd27b4f04b00f3aae18cd53279724c2b
PabloSantos-CG <comercial-pablo@outlook.com>
2025-06-03 20:25:44 -0300

    rename namespace

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Requeste;
+namespace App\Http;
 
 
 class Request
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Response;
+namespace App\Http;
 
 
 class Response
 {
     public static function json(array $data = [], int $status_code = 200)
     {
-        \http_response_code($status_code);
-        \header('Content-type: application/json');
+        http_response_code($status_code);
+        header('Content-type: application/json');
 
-        echo \json_encode($data);
+        echo json_encode($data);
     }
 }
