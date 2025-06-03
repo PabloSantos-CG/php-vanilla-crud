6b6faf1b8ef2b5be970ac2f5b1c07130d34675be
PabloSantos-CG <comercial-pablo@outlook.com>
2025-06-03 20:26:47 -0300

    refactor require path

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 use App\Core\Core;
 use App\Routes\Routes;
 
-require_once 'vendor/autoload.php';
-require_once 'src/Routes/routes.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/Routes/main.php';
 
 Core::run(Routes::routes());
