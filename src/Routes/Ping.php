7b1e964df9e97f28c6683079ead0dfe413b035d6
PabloSantos-CG <comercial-pablo@outlook.com>
2025-06-04 16:31:26 -0300

    refactor: change how use param

--- src/Routes/Ping.php
+++ src/Routes/Ping.php
@@ -10,7 +10,7 @@ class Ping
     public static function ping(Request $req, Response $res)
     {
         if (isset($_GET['url']) && $_GET['url'] === 'ping') {
-            $res::json(['ping' => 'pong']);
+            $res->json(['ping' => 'pong']);
         }
     }
 }
