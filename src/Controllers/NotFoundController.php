9de25f4de5ecb55c9671e8f62cb6b5a1a512f734
PabloSantos-CG <comercial-pablo@outlook.com>
2025-06-04 16:30:42 -0300

    refactor: change how use req param

--- src/Controllers/NotFoundController.php
+++ src/Controllers/NotFoundController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controllers;
 
+use App\Http\Request;
 use App\Http\Response;
 
 class NotFoundController
 {
-    public static function index()
+    public static function index(Request $req, Response $res)
     {
-        Response::json([
+        $res->json([
             'status' => 'error',
             'message' => 'Not Found',
         ], 404);
