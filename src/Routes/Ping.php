bb3e810ed22bb444dbbe24952e19ec330b8e5ee3
PabloSantos-CG <comercial-pablo@outlook.com>
2025-06-04 16:55:35 -0300

    refactor: add check method

--- src/Routes/Ping.php
+++ src/Routes/Ping.php
@@ -9,6 +9,12 @@ class Ping
 {
     public static function ping(Request $req, Response $res)
     {
+        if ($req->method() != 'GET') {
+            $res->json([
+                'status' => 'error',
+                'message' => 'Invalid Method',
+            ]);
+        }
         if (isset($_GET['url']) && $_GET['url'] === 'ping') {
             $res->json(['ping' => 'pong']);
         }
