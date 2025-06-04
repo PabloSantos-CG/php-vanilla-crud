a5328f4efa97a87d98cae15332f8763353d77f6e
PabloSantos-CG <comercial-pablo@outlook.com>
2025-06-04 16:43:37 -0300

    refactor: remove params

--- src/Controllers/NotFoundController.php
+++ src/Controllers/NotFoundController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controllers;
 
-use App\Http\Request;
 use App\Http\Response;
 
 class NotFoundController
 {
-    public static function index(Request $req, Response $res)
+    public static function index()
     {
+        $res = new Response();
+
         $res->json([
             'status' => 'error',
             'message' => 'Not Found',
