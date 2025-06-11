3eec56fdc9e0e7a3c178afaeb84c2681ed38c957
PabloSantos-CG <comercial-pablo@outlook.com>
2025-06-10 21:01:10 -0300

    refactor: add verification method

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -3,9 +3,31 @@
 namespace App\Core;
 
 use App\Controllers\NotFoundController;
+use App\Http\Request;
+use App\Http\Response;
 
 class Core
 {
+    /**
+     * @param array{
+     *     method: string,
+     *     path: string,
+     *     controller: callable
+     * } $route
+     */
+    private static function methodAllowed($route)
+    {
+        $req = new Request();
+        $res = new Response();
+        // echo $req->method() . $route['method'];
+        if ($req->method() !== $route['method']) {
+            $res->json([
+                'status' => 'error',
+                'message' => 'método de requisição inválido',
+            ], 404);
+        }
+    }
+
     /**
      * @param array<int, array{
      *     method: string,
@@ -23,13 +45,16 @@ class Core
         }
 
         $routeNotFound = true;
+
         foreach ($routes as $route) {
+
             $pattern = '#^' . preg_replace('#\{[^}]+\}#', '(\d+)', $route['path']) . '$#';
 
             if (preg_match($pattern, $url, $matches)) {
+                self::methodAllowed($route);
                 array_shift($matches);
                 call_user_func($route['controller'], ...$matches);
-                
+
                 $routeNotFound = false;
                 break;
             }
